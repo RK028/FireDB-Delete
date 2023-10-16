70afdc536293cd950104ccb44f1f93d215aae38b
Raj kumar <135219947+RK028@users.noreply.github.com>
2023-10-16 21:29:03 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAetxAevTHT1cHFL8kSM9ez_xzyOGjtoIQ",
-  authDomain: "curd-798f5.firebaseapp.com",
-  databaseURL: "https://curd-798f5-default-rtdb.firebaseio.com",
-  projectId: "curd-798f5",
-  storageBucket: "curd-798f5.appspot.com",
-  messagingSenderId: "145771089113",
-  appId: "1:145771089113:web:b249fcbf4ab8d64f5f02c8",
-  measurementId: "G-L65S574CND"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 
 // Initialize Firebase
